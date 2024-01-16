f4baac0d23a9b5fd4ff7aec97702b6d7a8fabcfb
Anastasia <anastasiia.dontsu@gmail.com>
2024-01-16 15:45:35 +0200

    changed and added routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Navbar from "components/Navbar";
-import Login from "components/Login";
 import Home from "components/Home";
 import Singup from "components/Singup";
+import Singin from "components/Singin";
 import Footer from "components/Footer";
 import User from "components/User";
 import Post from "components/Post";
@@ -13,8 +13,8 @@ const App = () => {
     <div>
       <Navbar />
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/Sign-Up" element={<Singup />} />
+        <Route path="/Sing-up" element={<Singup />} />
+        <Route path="/Sign-in" element={<Singin />} />
         <Route path="/home" element={<Home />} />
         <Route path="/user" element={<User />} />
         <Route path="/post" element={<Post />} />
