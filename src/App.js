09c26a996a28f0d22fa569d3c40c858f9870c36f
Anastasia <anastasiia.dontsu@gmail.com>
2024-01-19 15:24:47 +0200

    removed the component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Navbar from "components/Navbar";
-import Home from "components/Home";
 import Singup from "components/Singup";
 import Singin from "components/Singin";
 import Footer from "components/Footer";
 import User from "components/User";
 import Statistics from "components/Statistics";
+import Post from "components/Post";
 
 const App = () => {
   return (
@@ -15,9 +15,9 @@ const App = () => {
       <Routes>
         <Route path="/Sing-up" element={<Singup />} />
         <Route path="/Sing-in" element={<Singin />} />
-        <Route path="/Home" element={<Home />} />
         <Route path="/user" element={<User />} />
         <Route path="/statistics" element={<Statistics />} />
+        <Route path="/post" element={<Post />} />
       </Routes>
       <Footer
         title="Your Website Title"
