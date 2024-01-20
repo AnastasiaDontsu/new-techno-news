cd5825d3234bb363ea10b02bf8a9e7f0df22bb92
Anastasia <anastasiia.dontsu@gmail.com>
2024-01-20 04:16:15 +0200

    changed the component

---
+++ src/components/BitcoinNews.jsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from "react";
+import NewsCard from "../components/NewsCard";
+import { Grid, Container, Typography } from "@mui/material";
+
+function BitcoinNews() {
+  const [news, setNews] = useState([]);
+
+  const getNews = () => {
+    fetch(
+      `https://newsapi.org/v2/everything?q=bitcoin&apiKey=d663e9163be646dca11260deb7743314`
+    )
+      .then((res) => res.json())
+      .then((json) => setNews(json.articles.slice(0, 12)));
+  };
+
+  useEffect(() => {
+    getNews();
+  }, []);
+
+  return (
+    <div>
+      <Container
+        maxWidth="xl"
+        sx={{
+          bgcolor: "#e0e0e0",
+          padding: "10px",
+          marginBottom: "20px",
+        }}
+      >
+        <Typography
+          variant="h5"
+          component="div"
+          sx={{
+            textAlign: "center",
+            fontFamily: "'Playfair Display SC', serif",
+            padding: "10px",
+          }}
+        >
+          Сurrent news about Bitcoin
+        </Typography>
+        <Grid
+          container
+          spacing={2}
+          style={{ padding: "10px", marginBottom: "20px" }}
+        >
+          {news.map((data) => (
+            <Grid item xs={12} sm={4} md={4} lg={4} key={data.url}>
+              <NewsCard data={data} />
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </div>
+  );
+}
+
+export default BitcoinNews;
