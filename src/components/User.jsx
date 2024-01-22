5d22f7dd7a9415dfbcd727a78f144793bce4d9fe
Anastasia <anastasiia.dontsu@gmail.com>
2024-01-23 00:18:23 +0200

    changed and added a fetch

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,30 +1,26 @@
-import React, { useEffect, useState } from "react";
-import { DataGrid } from "@mui/x-data-grid";
+import React, { useState, useEffect } from "react";
 import Box from "@mui/material/Box";
-import Container from "@mui/material/Container";
-import DataTable from "utils/DataTable";
+import DataTable from "../utils/DataTable";
 
 export default function User() {
-  const rows = [
-    { id: 1, col1: "Hello", col2: "World" },
-    { id: 2, col1: "DataGridPro", col2: "is Awesome" },
-    { id: 3, col1: "MUI", col2: "is Amazing" },
-  ];
+  const [user, setUser] = useState([]);
 
   const columns = [
-    { field: "col1", headerName: "Column 1", width: 150 },
-    { field: "col2", headerName: "Column 2", width: 150 },
+    { field: "id", headerName: "ID", width: 150 },
+    { field: "name", headerName: "Name", width: 150 },
+    { field: "username", headerName: "Username", width: 150 },
+    { field: "email", headerName: "Email", width: 150 },
   ];
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((json) => console.log(json));
+      .then((json) => setUser(json));
   }, []);
 
   return (
-    <Box>
-      <DataTable rows={rows} columns={columns} />
+    <Box sx={{ height: 400, width: "100%" }}>
+      <DataTable rows={user} columns={columns} loading={!user.length} />
     </Box>
   );
 }
