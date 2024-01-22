41a344f2bb2acf5d620d2b6d1e897f692f2ed20a
Anastasia <anastasiia.dontsu@gmail.com>
2024-01-23 00:18:46 +0200

    cheated

--- src/utils/DataTable.jsx
+++ src/utils/DataTable.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { DataGrid } from "@mui/x-data-grid";
 
-const DataTable = ({ rows, columns }) => {
-  return <DataGrid rows={rows} columns={columns} />;
+const DataTable = ({ rows, columns, loading }) => {
+  return <DataGrid rows={rows} columns={columns} loading={loading} />;
 };
+
 export default DataTable;
