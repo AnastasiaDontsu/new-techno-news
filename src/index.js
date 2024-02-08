aad6e1601381baa19a57bd30329ff65b64501a05
Anastasia <anastasiadontsu21gmail.com>
2024-02-09 01:19:27 +0200

    removed unused code

--- src/index.js
+++ src/index.js
@@ -3,22 +3,12 @@ import ReactDOM from "react-dom/client";
 import "index.css";
 import App from "App";
 import { BrowserRouter } from "react-router-dom";
-import { Auth0Provider } from "@auth0/auth0-react";
-
-const domain = process.env.REACT_APP_DOMAIN;
-const clientId = process.env.REACT_APP_DOMAIN_CLIENT_ID;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Auth0Provider
-        domain={domain}
-        clientId={clientId}
-        redirectUri={window.location.origin}
-      >
-        <App />
-      </Auth0Provider>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
