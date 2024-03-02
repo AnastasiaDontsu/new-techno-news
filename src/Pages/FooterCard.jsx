03b2cf96ccc2a6b82670c31b82ddd7ad9bf65c11
Anastasia <anastasiadontsu21gmail.com>
2024-03-02 12:45:02 +0200

    deteted

--- src/Pages/FooterCard.jsx
+++
@@ -1,6 +0,0 @@
-import { Typography, styled } from "@mui/material";
-
-export const FooterCard = styled(Typography)(() => ({
-  textTransform: "uppercase", // Corrected typo
-  marginBottom: "1em",
-}));
