60a39b0282affc519b950bb12cfd7e1a8d28fe5e
Anastasia <anastasiadontsu21gmail.com>
2024-03-05 11:34:44 +0200

    add

--- src/components/Singup.tsx
+++ src/components/Singup.tsx
@@ -3,7 +3,6 @@ import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import {
   Button,
-  Container,
   Card,
   CardContent,
   Typography,
@@ -11,20 +10,22 @@ import {
   TextField,
   IconButton,
   InputAdornment,
+  Box,
+  Link,
+  Paper,
+  Container,
+  Stack,
 } from "@mui/material";
 
 import Visibility from "@mui/icons-material/Visibility";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import EmailIcon from "@mui/icons-material/Email";
-import PhoneEnabledIcon from "@mui/icons-material/PhoneEnabled";
 import AccountBoxIcon from "@mui/icons-material/AccountBox";
-import Box from "@mui/material/Box";
-import Link from "@mui/material/Link";
 import { useNavigate } from "react-router-dom";
+import PhoneEnabledIcon from "@mui/icons-material/PhoneEnabled";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase";
-
 interface ImageFile extends File {
   readonly preview: string;
 }
@@ -56,10 +57,6 @@ const Singup = () => {
     if (!password) {
       isproceed = false;
       errormessage += " password";
-    }
-    if (!phone) {
-      isproceed = false;
-      errormessage += " phone";
     }
 
     if (!isproceed) {
@@ -128,183 +125,194 @@ const Singup = () => {
   return (
     <>
       <Container
+        component="main"
         maxWidth="xl"
         sx={{
           backgroundImage:
-            "url(https://images.unsplash.com/photo-1550353127-b0da3aeaa0ca?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)",
+            "url(https://images.unsplash.com/photo-1496096265110-f83ad7f96608?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)",
           height: "100vh",
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
         }}
       >
-        <Card
+        <Box
+          display="flex"
+          justifyContent={"senter"}
+          flexDirection={"column"}
+          maxWidth={500}
+          alignItems="senter"
+          borderRadius={5}
+          padding={2}
+          boxShadow={"5px 5px 10px #ccc"}
           sx={{
-            maxWidth: 450,
-            margin: "0 auto",
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
+            backgroundColor: "#ffffff",
+            ":hover": {
+              boxShadow: "10px 10px 10px #ccc",
+            },
           }}
         >
-          <CardContent>
-            <Typography
-              sx={{ fontSize: 20, textAlign: "center", padding: "30px" }}
-            >
-              Registration
-            </Typography>
-            <form onSubmit={handleSubmit}>
-              <Grid container spacing={4}>
-                <Grid xs={12} md={6} item>
-                  <TextField
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                    label="Name"
-                    variant="outlined"
-                    id="name"
-                    InputProps={{
-                      endAdornment: (
-                        <InputAdornment position="end">
-                          <AccountCircleIcon />
-                        </InputAdornment>
-                      ),
-                    }}
-                  />
-                </Grid>
-                <Grid xs={12} md={6} item>
-                  <TextField
-                    value={lastname}
-                    onChange={(e) => setLastname(e.target.value)}
-                    label="Last name"
-                    variant="outlined"
-                    id="lastname"
-                    InputProps={{
-                      endAdornment: (
-                        <InputAdornment position="end">
-                          <AccountBoxIcon />
-                        </InputAdornment>
-                      ),
-                    }}
-                  />
-                </Grid>
-                <Grid xs={12} md={6} item>
-                  <TextField
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    label="Email"
-                    variant="outlined"
-                    id="email"
-                    InputProps={{
-                      endAdornment: (
-                        <InputAdornment position="end">
-                          <EmailIcon />
-                        </InputAdornment>
-                      ),
-                    }}
-                  />
-                </Grid>
-                <Grid xs={12} md={6} item>
-                  <TextField
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    id="password"
-                    type={showPassword ? "text" : "password"}
-                    label="Password"
-                    variant="outlined"
-                    InputProps={{
-                      endAdornment: (
-                        <InputAdornment position="end">
-                          <IconButton
-                            aria-label="toggle password visibility"
-                            onClick={handleClickShowPassword}
-                            onMouseDown={handleMouseDownPassword}
-                            edge="end"
-                          >
-                            {showPassword ? <VisibilityOff /> : <Visibility />}
-                          </IconButton>
-                        </InputAdornment>
-                      ),
-                    }}
-                  />
-                </Grid>
-                <Grid xs={12} md={6} item>
-                  <TextField
-                    value={phone}
-                    onChange={(e) => setPhone(e.target.value)}
-                    label="Phone"
-                    variant="outlined"
-                    id="phone"
-                    InputProps={{
-                      endAdornment: (
-                        <InputAdornment position="end">
-                          <PhoneEnabledIcon />
-                        </InputAdornment>
-                      ),
-                    }}
-                  />
-                </Grid>
-
-                <Box
-                  onClick={handleImageClick}
-                  sx={{
-                    borderRadius: "5%",
-                    cursor: "pointer",
-                    overflow: "hidden",
+          <Typography
+            sx={{
+              fontSize: 20,
+              textAlign: "center",
+              padding: "10px",
+              fontFamily: "'Playfair Display SC', serif",
+            }}
+          >
+            Registration
+          </Typography>
+          <form onSubmit={handleSubmit}>
+            <Grid container spacing={4}>
+              <Grid xs={12} sm={6} item>
+                <TextField
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  label="Name"
+                  variant="outlined"
+                  id="name"
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <AccountCircleIcon />
+                      </InputAdornment>
+                    ),
                   }}
-                >
-                  {image ? (
-                    <img
-                      src={URL.createObjectURL(image)}
-                      alt="Remy"
-                      style={{
-                        width: "200px",
-                        height: "112.5px",
-                        objectFit: "contain",
-                        objectPosition: "center top",
-                      }}
-                    />
-                  ) : (
-                    <img
-                      src="https://img.freepik.com/premium-vector/female-user-profile-avatar-is-a-woman-a-character-for-a-screen-saver-with-emotions_505620-617.jpg"
-                      alt="Remy"
-                      style={{
-                        width: "200px",
-                        height: "112.5px",
-                        objectFit: "contain",
-                        objectPosition: "center top",
-                      }}
-                    />
-                  )}
-                  <input
-                    type="file"
-                    ref={inputRef}
-                    onChange={handleImageChange}
-                    style={{ display: "none" }}
-                  ></input>
-                </Box>
+                  sx={{ display: "block" }}
+                />
+              </Grid>
+              <Grid xs={12} sm={6} item>
+                <TextField
+                  value={lastname}
+                  onChange={(e) => setLastname(e.target.value)}
+                  label="Last name"
+                  variant="outlined"
+                  id="lastname"
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <AccountBoxIcon />
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+              </Grid>
+              <Grid xs={12} sm={6} item>
+                <TextField
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  label="Email"
+                  variant="outlined"
+                  id="email"
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <EmailIcon />
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+              </Grid>
+              <Grid xs={12} sm={6} item>
+                <TextField
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  id="password"
+                  type={showPassword ? "text" : "password"}
+                  label="Password"
+                  variant="outlined"
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label="toggle password visibility"
+                          onClick={handleClickShowPassword}
+                          onMouseDown={handleMouseDownPassword}
+                          edge="end"
+                        >
+                          {showPassword ? <VisibilityOff /> : <Visibility />}
+                        </IconButton>
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+              </Grid>
+              <Grid xs={12} md={6} item>
+                <TextField
+                  value={phone}
+                  onChange={(e) => setPhone(e.target.value)}
+                  label="Phone"
+                  variant="outlined"
+                  id="phone"
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <PhoneEnabledIcon />
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+              </Grid>
+              <Box
+                onClick={handleImageClick}
+                sx={{
+                  borderRadius: "5%",
+                  cursor: "pointer",
+                  overflow: "hidden",
+                }}
+              >
+                {image ? (
+                  <img
+                    src={URL.createObjectURL(image)}
+                    alt="Remy"
+                    style={{
+                      width: "200px",
+                      height: "112.5px",
+                      objectFit: "contain",
+                      objectPosition: "center top",
+                    }}
+                  />
+                ) : (
+                  <img
+                    src="https://img.freepik.com/premium-vector/female-user-profile-avatar-is-a-woman-a-character-for-a-screen-saver-with-emotions_505620-617.jpg"
+                    alt="Remy"
+                    style={{
+                      width: "200px",
+                      height: "112.5px",
+                      objectFit: "contain",
+                      objectPosition: "center top",
+                    }}
+                  />
+                )}
+                <input
+                  type="file"
+                  ref={inputRef}
+                  onChange={handleImageChange}
+                  style={{ display: "none" }}
+                ></input>
+              </Box>
 
-                <Grid
-                  xs={12}
-                  item
-                  sx={{ textAlign: "center", marginTop: "-30px" }}
+              <Grid
+                xs={12}
+                item
+                sx={{ textAlign: "center", marginTop: "-50px" }}
+              >
+                <Button
+                  sx={{ backgroundColor: "success.main" }}
+                  type="submit"
+                  variant="contained"
                 >
-                  <Button
-                    sx={{ backgroundColor: "success.main" }}
-                    type="submit"
-                    variant="contained"
-                  >
-                    Sign-up
-                  </Button>
-                </Grid>
+                  Sign-up
+                </Button>
               </Grid>
-              <Grid sx={{ textAlign: "center", padding: "15px" }} item>
-                <Link href="Sing-in" variant="body2">
-                  {"Don't have an account? Sing in"}
-                </Link>
-              </Grid>
-            </form>
-          </CardContent>
-        </Card>
+            </Grid>
+            <Grid sx={{ textAlign: "center", padding: "15px" }} item>
+              <Link href="Sing-in" variant="body2">
+                {"Don't have an account? Sing in"}
+              </Link>
+            </Grid>
+          </form>
+        </Box>
       </Container>
     </>
   );
