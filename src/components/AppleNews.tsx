f00242449c6734c6231b1668a1273fca4c39f0fd
Anastasia <anastasiadontsu21gmail.com>
2024-05-10 23:36:34 +0300

    add

--- src/components/AppleNews.tsx
+++ src/components/AppleNews.tsx
@@ -9,55 +9,47 @@ interface AppleNewsProps {
 }
 
 function AppleNews() {
-  const [news, setNews] = useState<AppleNewsProps[]>([]);
+  const [name, setName] = useState<string>("Ana");
+  const [showGreeting, setShowGreeting] = useState<boolean>(false);
+  const [names, setNames] = useState<{ id: number; name: string }[]>([]);
 
-  const getNews = () => {
-    fetch(
-      `https://newsapi.org/v2/everything?q=apple&from=2024-03-01&to=2024-03-01&sortBy=popularity&apiKey=9acbff4c96d64f10981243ce862345b3`
-    )
-      .then((res) => res.json())
-      .then((json) => setNews(json.articles.slice(3, 15)));
-  };
+  function handleClick() {
+    console.log("click");
+    setShowGreeting(!showGreeting);
+  }
+
+  function skrinContr(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    setNames([...names, { id: names.length, name }]);
+    setName("");
+  }
 
   useEffect(() => {
-    getNews();
-  }, []);
+    console.log("Name changed:", name);
+  }, [name]);
 
   return (
     <div>
-      <Container
-        maxWidth="xl"
-        sx={{
-          padding: "10px",
-          marginBottom: "20px",
-        }}
-      >
-        <Typography
-          variant="h5"
-          component="div"
-          sx={{
-            textAlign: "center",
-            fontFamily: "'Playfair Display SC', serif",
-            padding: "10px",
+      <div>Hello, {showGreeting ? name : ""}</div>
+      <button onClick={handleClick}>Click me!</button>
+      <hr></hr>
+      <form onSubmit={skrinContr}>
+        <input
+          type="text"
+          placeholder="hello"
+          value={name}
+          onChange={(e) => {
+            setName(e.target.value);
           }}
-        >
-          Current news about Apple
-        </Typography>
-        <Grid
-          container
-          spacing={2}
-          sx={{
-            padding: { xs: 2, sm: 3 },
-            marginBottom: "20px",
-          }}
-        >
-          {news.map((data, i) => (
-            <Grid item xs={12} sm={6} md={4} lg={4} key={i}>
-              <AppleCard data={data} />
-            </Grid>
-          ))}
-        </Grid>
-      </Container>
+        />
+        <button>Submit</button>
+      </form>
+      <hr></hr>
+      <ul>
+        {names.map((item) => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
