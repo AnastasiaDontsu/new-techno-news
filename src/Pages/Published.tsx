24413bfb585a6e6c2bc2bfc98b12f44870738d76
Anastasia <anastasiadontsu21gmail.com>
2024-05-10 23:35:18 +0300

    changes

--- src/Pages/Published.tsx
+++ src/Pages/Published.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, CardMedia, CardContent } from "@mui/material";
+import { Box, CardMedia, CardContent, Link } from "@mui/material";
 import Card from "@mui/material/Card";
 import Typography from "@mui/material/Typography";
 
@@ -12,17 +12,20 @@ interface PublishedProps {
 }
 
 const Published: React.FC<PublishedProps> = ({ data }) => {
-  console.log(data);
-
   return (
     <>
       <Box>
-        <Card>
-          <CardMedia height="300" component="img" image={data.urlToImage} />
-          <CardContent>
-            <Typography sx={{ fontWeight: "bold" }}>{data.title}</Typography>
-          </CardContent>
-        </Card>
+        <CardMedia component="img" height="250" image={data.urlToImage} />
+        <CardContent>
+          <Typography variant="h5" sx={{ fontWeight: "bold" }}>
+            {data.title}
+          </Typography>
+          <Typography>
+            <Link href={data.url} target="_blank" rel="noopener noreferrer">
+              Read more
+            </Link>
+          </Typography>
+        </CardContent>
       </Box>
     </>
   );
